4befe90685071882295b951d89cb24730c888613
ispira <ispiralabs@gmail.com>
2018-03-27 21:38:16 +0200

    frontend skeleton

---
+++ frontend/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+    './assets/scss/index.scss'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/build')
+  },
+  module: {
+    rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'bundle.css',
+              }
+            },
+            {
+              loader: 'extract-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ]
+  }
+};
